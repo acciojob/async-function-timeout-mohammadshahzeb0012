38f61a827a785226f94c230758e6c6261db3b48c
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-02-23 23:48:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+const form = document.querySelector("form");
+const btn = document.getElementById("btn")
+const output = document.querySelector("#output")
+
+btn.addEventListener("click",()=>{
+  const text = document.getElementById("text").value;
+  const delay = document.getElementById("delay").value
+
+  const promise1 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      resolve(`${text} - ${delay}`)
+    },delay)
+  })
+  
+  const asyncHandler =  ( async ()=>{
+    try{
+      const result = await  promise1;
+      output.innerText = `${result}`
+      return
+    }
+    catch(err){
+      alert(err)
+      return
+    }
+  })
+   asyncHandler()
+
+})
+
